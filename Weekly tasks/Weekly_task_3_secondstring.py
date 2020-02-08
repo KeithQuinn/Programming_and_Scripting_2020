6ea7f6652bd431266a0223190caa1ff244cdc2ca
Quinn <Keith.Quinn@bsci.com>
2020-02-08 17:09:15 +0000

    Uploading week 1 to week 4 tasks, new naming format and folder structure

---
+++ Weekly tasks/Weekly_task_3_secondstring.py
@@ -0,0 +1,5 @@
+#Input sentence, output every second letter in reverse order
+
+sentence = input("enter sentence: ")
+
+print(sentence [::-2])
--- Weekly tasks/Weekly_task_4_collatz.py
+++ Weekly tasks/Weekly_task_4_collatz.py
@@ -11,14 +11,14 @@ while p > 1:
     
      p = p / 2
 
-     print("the new value of p is",p)
+     print("the new value is",p)
 
     else:
         print(p, "is not an even number and so it is multiplied by 3 and 1 is added")
     
         p = ((p * 3) + 1)
 
-        print("the new value of p is",p)
+        print("the new value is",p)
 
 
 
