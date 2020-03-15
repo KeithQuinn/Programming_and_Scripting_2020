09bd4ae109e1028296287e6a0df942b448db5bb6
Quinn <Keith.Quinn@bsci.com>
2020-03-15 17:19:45 +0000

    Adding week 8 task

---
+++ Weekly tasks/Weekly_task_8.plotting.py
@@ -0,0 +1,15 @@
+# IPython log file
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0.0, 4.0, 40)
+g = x**2
+h = x**3
+plt.plot(x, g, "y.", label = "Squared")
+plt.plot(x, h, "gx", label = "Cubed")
+plt.title("Squared vs Cubed")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.legend()
+plt.savefig("Weekly_task_8.png")
+plt.show()
