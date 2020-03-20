78e7c5d9eeb2751416eab49d233c62613095b0c2
Quinn <Keith.Quinn@bsci.com>
2020-03-20 17:19:31 +0000

    Updating weekly task 8 to include f(x), also adding show, pause, close to code

--- Weekly tasks/Weekly_task_8.plotting.py
+++ Weekly tasks/Weekly_task_8.plotting.py
@@ -5,11 +5,14 @@ import numpy as np
 x = np.linspace(0.0, 4.0, 40)
 g = x**2
 h = x**3
-plt.plot(x, g, "y.", label = "Squared")
-plt.plot(x, h, "gx", label = "Cubed")
-plt.title("Squared vs Cubed")
+plt.plot(x, x, "r.", label = "f(x) = x")
+plt.plot(x, g, "y*", label = "g(x) = $x^2$")
+plt.plot(x, h, "gx", label = "h(x) = $x^3$")
+plt.title("Weekly_Task_8_Plotting")
 plt.xlabel("x-axis")
 plt.ylabel("y-axis")
 plt.legend()
 plt.savefig("Weekly_task_8.png")
-plt.show()
+plt.show(block=False)
+plt.pause(5)
+plt.close("all")
